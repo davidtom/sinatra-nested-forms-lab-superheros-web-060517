2d17ecde66f48617918d7d498aebb6814459d028
davidtom <davidjtomczyk@gmail.com>
2017-07-03 09:31:15 -0400

    complete lab

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,16 @@ require 'sinatra/base'
 class App < Sinatra::Base
 
     set :views, Proc.new { File.join(root, "../views/") }
+    # set :views, "./app/views"
 
+    get "/" do
+      erb :super_hero
+    end
+
+    post "/teams" do
+      # binding.pry
+      @team_info = params[:team]
+      erb :team
+    end
 
 end
